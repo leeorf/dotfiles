2e906ed035dd33ce78bfc06c704f72f9d5811301
Leonardo Rodrigues Ferreira <leo.rf.developer@gmail.com>
2024-08-31 13:12:25 -0300

    Add initial setup for bash so that all configs are successfully loaded

--- bash/settings.bash
+++ bash/settings.bash
@@ -1,4 +1,11 @@
+# This is necessary to avoid "Unknown locale, assuming C - Error message
+# in terminal"
+export LANG="en_US.UTF-8"
+
 # Vim emulation
 set -o vi
 
 export EDITOR=vim
+
+# Load brew if present
+eval "$(/opt/homebrew/bin/brew shellenv)" 2>/dev/null
